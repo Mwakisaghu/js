dc09b9823b73536e648c24677e6d77707fa59562
mwakisaghu <harrisonchambela2016@gmail.com>
2021-05-25 16:16:38 +0300

    This will help fetch and validate the user input

--- dom/dom-demo project/assets/scripts/app.js
+++ dom/dom-demo project/assets/scripts/app.js
@@ -9,7 +9,12 @@ const backdrop = document.getElementById('backdrop');
 
 const cancelAddMovieButton = document.querySelector('.btn--passive');
 //const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
-//const addCancelMovieButton = document.querySelector('.btn--success');
+
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+//const confirmAddMovieButton = document.querySelector('.btn--success');
+
+const userInputs = addMovieModal.querySelectorAll('input');
+//const userInputs = addMovieModal.getElementsByTagName('input');
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -20,19 +25,30 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
 };
 
-/*const addCancelMovie = () => {
-  toggleMovieModal();
-};*/
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (titleValue.trim() === [0] || 
+  imageUrlValue.trim() === '' || 
+  ratingValue.trim() === '' || 
+  +ratingValue < 1 ||
+  +ratingValue > 5 
+  ) {
+    alert('Please enter valid valuues (rating between 1 and 5)');
+  }
+};
 
 const backdropClickHandler = () => {
   toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
-backdrop.addEventListener('click',toggleMovieModal);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
-//addCancelMovieButton.addEventListener('click', addCancelMovie);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
