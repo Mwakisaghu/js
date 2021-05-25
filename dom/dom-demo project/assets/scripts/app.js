f67bf2971ca055bc40425a618e57ffb9cec3c040
mwakisaghu <harrisonchambela2016@gmail.com>
2021-05-25 15:30:50 +0300

    This will control the backdrop

--- dom/dom-demo project/assets/scripts/app.js
+++ dom/dom-demo project/assets/scripts/app.js
@@ -4,3 +4,35 @@ const addMovieModal = document.getElementById('add-modal');
 
 //const startAddMovieButton = document.querySelector('header').lastElementChild;
 const startAddMovieButton = document.querySelector('header button');
+
+const backdrop = document.getElementById('backdrop');
+
+const cancelAddMovieButton = document.querySelector('.btn--passive');
+//const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+//const addCancelMovieButton = document.querySelector('.btn--success');
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+/*const addCancelMovie = () => {
+  toggleMovieModal();
+};*/
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click',toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+//addCancelMovieButton.addEventListener('click', addCancelMovie);
