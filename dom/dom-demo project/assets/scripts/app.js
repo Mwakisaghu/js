6e60a8956ebd0353e24c3e4572edf231a7a3e7d7
mwakisaghu <harrisonchambela2016@gmail.com>
2021-05-25 14:32:47 +0300

    selecting the modal and Add Button using js

---
+++ dom/dom-demo project/assets/scripts/app.js
@@ -0,0 +1,6 @@
+const addMovieModal = document.getElementById('add-modal');
+//const addMovemodal = document.querySelector('#add-modal');
+//const addMovieModal = document.body.children[1];
+
+//const startAddMovieButton = document.querySelector('header').lastElementChild;
+const startAddMovieButton = document.querySelector('header button');
