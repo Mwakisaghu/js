e27e9de3b1a523eb3cd3fbbcbcefdb3d54669163
mwakisaghu <harrisonchambela2016@gmail.com>
2021-06-15 06:42:38 +0300

    working with maps

--- Arrays & Iterables/creating-arrays/setmaps.js
+++ Arrays & Iterables/creating-arrays/setmaps.js
@@ -1,4 +1,6 @@
-const ids = new Set(['hello', 'from', 'to']);
+//WORKING WITH SETS......................................................
+
+/*const ids = new Set(['hello', 'from', 'to']);
 ids.add(2)
 //console.log(ids.has(2));
 
@@ -9,4 +11,27 @@ if (ids.has('hello')) {
 
 for(const entry of ids.entries()) {
   console.log(entry);
-}
+}*/
+
+//WORKING WITH MAPS.......................................................
+const person1 ={name: 'Harry'};
+const person2 = {name: 'codedgenius'};
+
+const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+
+personData.set(person2, [{date: 'two weeks earlier', price: 100}]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for(const entry of personData.entries()) {
+  console.log(entry);
+};
+
+for(const [key, value] of personData.entries()) {
+  console.log(key, value);
+};
+
+for(const key of personData.keys()) {
+  console.log(key);
+};
