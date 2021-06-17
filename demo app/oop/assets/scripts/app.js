64ba47d6ad6fff8cdd531e3dec574e41be44f587
mwakisaghu <harrisonchambela2016@gmail.com>
2021-06-17 06:52:15 +0300

    Working with the constructor method

--- demo app/oop/assets/scripts/app.js
+++ demo app/oop/assets/scripts/app.js
@@ -3,27 +3,38 @@ class Product {
   imageUrl;
   price;
   description;
-}
 
-console.log(new Product());
+  constructor(title, image, price, desc) {
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = desc;
+  }
+}
 
 const productList = {
   products: [
-    //new Product(),
-    {
+    new Product(
+      'A Pillow',
+      'https://cdn.shopify.com/s/files/1/0150/0643/3380/products/Viacom_Spongebob_Pillow16inX16inPRTGENSLG16B_00043_800x.jpg?v=1563223169',
+      19.99,
+      'A soft pillow!'
+    ),
+    /*{
       title: 'A Pillow',
       imageUrl:
         'https://cdn.shopify.com/s/files/1/0150/0643/3380/products/Viacom_Spongebob_Pillow16inX16inPRTGENSLG16B_00043_800x.jpg?v=1563223169',
       price: 19.99,
       description: 'A soft pillow!',
-    },
-    {
+    },*/
+    new Product('A Carpet', 'https://ke.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/95/994803/1.jpg?7448', 89.99, 'A clean and nice carpet which you might like!')
+    /*{
       title: 'A Carpet',
       imageUrl:
         'https://ke.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/95/994803/1.jpg?7448',
       price: 89.99,
       description: 'A clean and nice carpet which you might like!',
-    },
+    },*/
   ],
   render() {
     const renderHook = document.getElementById('app');
