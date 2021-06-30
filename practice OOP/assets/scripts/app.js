eff4c1ff4561188acbceb6f5cc698c7335e95f7d
mwakisaghu <harrisonchambela2016@gmail.com>
2021-06-30 15:57:42 +0300

    this show the 'switch-project' logic

--- practice OOP/assets/scripts/app.js
+++ practice OOP/assets/scripts/app.js
@@ -1,19 +1,42 @@
-class Tooltip {};
+class Tooltip {}
 
-class ProjectItem {};
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectSwitchButton();
+  }
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = document.querySelector('button: last-of-type');
+    switchBtn.addEventListener('click');
+  }
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
-    const prjItems = document.querySelectorAll(`${type}-projects li`);
-    console.log(prjItems);
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
   }
-};
+  addProject() {}
+  switchProject() {
+    //const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    //this.projects.splice(projectIndex, 1);
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
 
 class App {
   static init() {
     const activeProjectList = new ProjectList('active');
     const finishedProjectList = new ProjectList('finished');
   }
-};
+}
 
 App.init();
