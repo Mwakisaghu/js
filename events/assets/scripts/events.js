7019b939f9840ebcc0cec438f41f92fd56fcd527
mwakisaghu <harrisonchambela2016@gmail.com>
2021-07-21 14:15:04 +0300

    this shows different ways of adding event listeners

---
+++ events/assets/scripts/events.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('button')
+
+/*const buttonClickHandler = () => {
+  alert('This button was clicked!');
+};
+
+const anotherButtonHandler = () => {
+  alert('You clicked this button!');
+};
+
+button.onclick = buttonClickHandler;
+button.onclick = anotherButtonHandler;*/
+
+button.addEventListener();
+
+//button.removeEventListener();
