450fbc0f9249b9aa4ef2fb2b7135249aa152414f
mwakisaghu <harrisonchambela2016@gmail.com>
2021-07-21 14:32:38 +0300

    removing event listeners

--- events/assets/scripts/events.js
+++ events/assets/scripts/events.js
@@ -1,6 +1,6 @@
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 
-/*const buttonClickHandler = () => {
+const buttonClickHandler = () => {
   alert('This button was clicked!');
 };
 
@@ -8,9 +8,19 @@ const anotherButtonHandler = () => {
   alert('You clicked this button!');
 };
 
-button.onclick = buttonClickHandler;
-button.onclick = anotherButtonHandler;*/
+//button.onclick = buttonClickHandler;
+//button.onclick = anotherButtonHandler;
 
-button.addEventListener();
+//const boundFn = buttonClickHandler.bind(this);
+
+button.addEventListener('click', () => {
+  console.log('Clicked!');
+});
+
+setTimeout(() => {
+  button.removeEventListener('click', () => {
+    console.log('Clicked!');
+  });
+}, 2000);
 
 //button.removeEventListener();
