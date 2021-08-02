83193a8628c2bbbcb0b0791725fa1301b19fe37b
mwakisaghu <harrisonchambela2016@gmail.com>
2021-08-02 14:19:34 +0300

    Tagged Templates

--- NO.strings/app.js
+++ NO.strings/app.js
@@ -4,3 +4,21 @@ function randomInBetween(min, max) {
 }
 
 console.log(randomInBetween(1, 10));
+
+//Tagged Templates
+function productDescription(strings, productName, productPrice) {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  let pricecategory = 'pretty cheap';
+  if (productPrice > 20) {
+    priceCategory = 'fairly priced';
+  }
+  return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+}
+
+const prodName = 'JavaScript course';
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}`;
+console.log(productOutput);
