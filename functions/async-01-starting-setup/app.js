32139a466c4dcc487c76e8d7c9d05639c5d74dee
mwakisaghu <harrisonchambela2016@gmail.com>
2021-08-04 14:13:02 +0300

    this shows how to chain multiple promises

--- functions/async-01-starting-setup/app.js
+++ functions/async-01-starting-setup/app.js
@@ -1,10 +1,23 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (sucess) => {
+        resolve(sucess);
+      },
+      (error) => {},
+      opts
+    );
+  });
+  return promise;
+};
+
 const setTimer = (duration) => {
-  const promise = new Promise((reslove, reject) => {
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      reslove('Done!');
+      resolve('Done!');
     }, duration);
   });
   return promise;
@@ -12,16 +25,24 @@ const setTimer = (duration) => {
 
 function trackUserHandler() {
   //console.log('Clicked!');
-  navigator.geolocation.getCurrentPosition(
-    (posData) => {
+  //navigator.geolocation.getCurrentPosition(
+  let positionData;
+  getPosition()
+    .then((posData) => {
+      positionData = posData;
+      return setTimer(2000);
+    })
+    .then((data) => {
+      console.log(data, positionData);
+    });
+  /*(posData) => {
       setTimer(2000).then((data) => {
         console.log(data, posData);
       });
     },
     (error) => {
       console.log(error);
-    }
-  );
+    }*/
   setTimer(1000).then(() => {
     console.log('Timer done!');
   });
