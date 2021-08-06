bb91db7f6675d8cd363454c094f5f4e9f1aec936
mwakisaghu <harrisonchambela2016@gmail.com>
2021-08-06 14:56:26 +0300

    This shows how to promisify Http Requests with XMLHttpRequest

--- http/assets/scripts/app.js
+++ http/assets/scripts/app.js
@@ -1,23 +1,36 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequests(method, url) {
+  const xhr = new XMLHttpRequest();
+  const promise = new Promise((resolve, reject) => {
+    xhr.open(method, url);
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.responseType = 'json';
 
-xhr.responseType = 'json';
+    xhr.onload = function () {
+      resolve(xhr.response);
+      //console.log(xhr.response);
+      //const listsOfPosts = JSON.parse(xhr.response);
+    };
 
-xhr.onload = function () {
-  //console.log(xhr.response);
-  //const listsOfPosts = JSON.parse(xhr.response);
-  const listsOfPosts = xhr.response;
-  console.log(listsOfPosts);
+    xhr.send();
+  });
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequests(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  const listsOfPosts = responseData;
   for (const post of listsOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
     listElement.append(postEl);
   }
-};
+}
 
-xhr.send();
+fetchPosts();
