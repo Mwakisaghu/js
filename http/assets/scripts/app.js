cf5112aca600dde0b17a00b82b5b6c76e4f370c2
mwakisaghu <harrisonchambela2016@gmail.com>
2021-08-06 14:12:48 +0300

    This shows how to parse JSON data

--- http/assets/scripts/app.js
+++ http/assets/scripts/app.js
@@ -1,5 +1,23 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+xhr.responseType = 'json';
+
+xhr.onload = function () {
+  //console.log(xhr.response);
+  //const listsOfPosts = JSON.parse(xhr.response);
+  const listsOfPosts = xhr.response;
+  console.log(listsOfPosts);
+  for (const post of listsOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
+  }
+};
 
 xhr.send();
